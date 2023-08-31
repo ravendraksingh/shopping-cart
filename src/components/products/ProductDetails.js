b07c30fef8889b35478d40ed03b392a464d1c47b
ravendraksingh <ravendraksingh@gmail.com>
2023-08-31 12:15:36 +0530

    modifed layout

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./product.css";
 
 const ProductDetails = () => {
   const [product, setProduct] = useState({});
@@ -24,11 +25,22 @@ const ProductDetails = () => {
   }, []);
 
   return (
-    <div className="container fs-4">
-      ProductDetails
+    <div className="container mt-1 fs-4">
       <img src={product.thumbnail} alt={product.title} className="w-100" />
-      <p className="">{product.description}</p>
-      <p>Brand: {product.brand}</p>
+      <div className="mt-2">
+        <p className="">{product.description}</p>
+        <span>Brand: {product.brand}</span>
+        <span className="float-end">Price: ${product.price}</span>
+        <p>
+          <span>Discount: {product.discountPercentage}%</span>
+          <span className="float-end">Rating: {product.rating}</span>
+        </p>
+      </div>
+      <div className="other__img_container">
+        {product?.images?.map((i, index) => (
+          <img src={i} alt={"lp2nb2#" + index} className="other__img" />
+        ))}
+      </div>
     </div>
   );
 };
